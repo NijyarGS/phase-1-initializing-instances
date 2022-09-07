e5c6f7afb747487ac05fec7b7f8ff58b66b49eca
Nijyar Ghazi <nijyar360@gmail.com>
2022-09-07 15:18:55 +0300

    Completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your code here
+class breakfast{
+    constructor(food,drink)
+    {
+        this.food = food;
+        this.drink = drink;
+    }
+}
+
+class lunch{
+    constructor(salad,soup,drink)
+    {
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+
+}
+
+class dinner{
+    #dessert;
+    constructor(salad,soup,entree,dessert){
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this.#dessert = dessert;
+    }
+
+}
+// salad, soup, entree, and dessert. 
+//Initialize dessert as a private
